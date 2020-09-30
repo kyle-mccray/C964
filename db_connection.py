fbbc5e441b60437c3e5e2194262e931f774ae408
kyle <kcmsonic@gmail.com>
2020-09-30 16:57:48 -0500

    updated db connection for heroku

--- db_connection.py
+++ db_connection.py
@@ -7,14 +7,14 @@ class Connection:
     def __init__(self):
         try:
             print("Trying Connection to DB")
-            # DATABASE_URL = os.environ['DATABASE_URL']
-            # conn = psycopg2.connect(DATABASE_URL, sslmode='require', cursor_factory=DictCursor)
-            conn = psycopg2.connect(
-                host="localhost",
-                database="flask",
-                user="server",
-                password="admin",
-                cursor_factory=DictCursor)
+            DATABASE_URL = os.environ['DATABASE_URL']
+            conn = psycopg2.connect(DATABASE_URL, sslmode='require', cursor_factory=DictCursor)
+            # conn = psycopg2.connect(
+            #     host="localhost",
+            #     database="flask",
+            #     user="server",
+            #     password="admin",
+            #     cursor_factory=DictCursor)
             cur = conn.cursor()
             self.cur = cur
             print("Successfully Connected")
